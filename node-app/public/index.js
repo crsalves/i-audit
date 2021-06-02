e2f67f1262e1df9c047dece58f672f3d1fd38c7f
carla <carlarobertasilva@hotmail.com>
2021-06-02 12:07:35 -0400

    added delete route

--- node-app/public/index.js
+++ node-app/public/index.js
@@ -1,25 +1,40 @@
-alert("bingo");
+// Activate tooltip
+$('[data-toggle="tooltip"]').tooltip();
 
-$(document).ready(function(){
-    // Activate tooltip
-    $('[data-toggle="tooltip"]').tooltip();
+// Select/Deselect checkboxes
+var checkbox = $('table tbody input[type="checkbox"]');
+$("#selectAll").click(function () {
+    if (this.checked) {
+        checkbox.each(function () {
+            this.checked = true;
+        });
+    } else {
+        checkbox.each(function () {
+            this.checked = false;
+        });
+    }
+});
+checkbox.click(function () {
+    if (!this.checked) {
+        $("#selectAll").prop("checked", false);
+    }
+});
 
-    // Select/Deselect checkboxes
-    var checkbox = $('table tbody input[type="checkbox"]');
-    $("#selectAll").click(function(){
-        if(this.checked){
-            checkbox.each(function(){
-                this.checked = true;
-            });
-        } else{
-            checkbox.each(function(){
-                this.checked = false;
-            });
-        }
-    });
-    checkbox.click(function(){
-        if(!this.checked){
-            $("#selectAll").prop("checked", false);
+// bind this event to all delete buttons
+$(".delete").click(function(){
+    // find the modal body
+    var modal = $("#deleteTransaction").find(".modal-body");
+
+    // loop through all the check boxes (class checkbox)
+    $(".checkbox").each(function(index){
+
+        // if they are checked, add them to the modal
+        var transaction_id = $(this).val();
+
+        if($(this).is(":checked")){
+            // add a hidden input element to modal with article ID as value
+            $(modal).append("<input name='transaction_id' value='"+ transaction_id +"'  type='hidden' />")
         }
     });
 });
+
