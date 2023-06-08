247c3599d9c0444cb48c6166e548f48ee7a29365
Carla Alves <carlaalves.crs@gmail.com>
2023-06-08 17:29:28 -0400

    fixed appended transaction ids

--- node-app/public/js/index.js
+++ node-app/public/js/index.js
@@ -1,18 +1,11 @@
-
-
-$(".delete").click(function () {
-    var modal = $("#deleteTransaction").find(".modal-body");
-    var transaction_id = $(this).attr("id");
-    $(modal).append("<input type='text' class='myClassTest' name='transaction_id' value='" + transaction_id + "' hidden/>")
-});
-
-function reply_click(event) {
-    var id = event.id;
+function populate_modal_window(event) {
+    var transaction_id = event.id;
 
     $("table > tbody > tr").each(function () {
         var row = $(this);
-
-        if (row.find(".transaction_id").text() == id) {
+        var row_id = $(this).attr("id")
+        if (row_id == transaction_id) {
+            // Date
             var transaction_date = row.find(".transaction_date").text()
             var date = new Date(transaction_date);
             var yr = date.getFullYear();
@@ -36,8 +29,8 @@ function reply_click(event) {
             var excelDateString = newDateString + 'T' + newTimeString;
             $("#transactionDate").val(excelDateString);
 
-
-            var category_type = row.find(".category_type").text()
+            // category
+            var category_type = row.find(".category_type").text().trim()
             switch (category_type) {
                 case "Salary":
                     $("#editTransaction").find(".modal-body input:radio[name=category_type]").filter('[value=1]').prop('checked', true);
@@ -65,26 +58,43 @@ function reply_click(event) {
             }
 
 
-            var transaction_type = row.find(".transaction_type").text()
-            if (transaction_type == "Debit") {
+            // type of transaction
+            var transaction_type = row.find(".transaction_type").text().trim()
+            if (transaction_type === "Debit") {
                 $("#editTransaction").find(".modal-body input:radio[name=transaction_type]").filter('[value=1]').prop('checked', true);
             } else if (transaction_type == "Credit") {
                 $("#editTransaction").find(".modal-body input:radio[name=transaction_type]").filter('[value=2]').prop('checked', true);
             }
 
-
+            // transaction value
             var transaction_value = row.find(".transaction_value").text()
+
             // This omits the negative sign from the string
             if (transaction_value.substring(0, 1) == "-") {
                 transaction_value = transaction_value.substring(1);
             }
             $("#editTransaction").find(".modal-content input:text[name=transaction_value]").prop('value', transaction_value);
-
         }
     });
+
 }
 
+$(".edit").click(function () {
+    var modal = $("#editTransaction").find(".modal-body");
+    var transaction_id = $(this).attr("id");
+    $(modal).append("<input type='text' class='transactionEditAppend' name='transaction_id' value='" + transaction_id + "' hidden/>")
+});
 
+function remove_transaction_id_edit_append() {
+    $("#editTransaction").find(".modal-body input:text[name=transaction_id]").remove();
+}
 
+$(".delete").click(function () {
+    var modal = $("#deleteTransaction").find(".modal-body");
+    var transaction_id = $(this).attr("id");
+    $(modal).append("<input type='text' class='transactionDeleteAppend' name='transaction_id' value='" + transaction_id + "' hidden/>")
+});
 
-
+function remove_transaction_id_delete_append() {
+    $("#deleteTransaction").find(".modal-body input:text[name=transaction_id]").remove();
+}
--- scripts/i-audit-data.sql
+++ scripts/i-audit-data.sql
@@ -2,6 +2,7 @@ INSERT INTO i_audit.transaction (account_id, transaction_type_id, category_type_
 VALUES (4, 2, 10, '2023-06-05 14:26:00', 7000);
 
 
+
 INSERT INTO i_audit.transaction (account_id, transaction_type_id, category_type_id, transaction_date, transaction_value)
 VALUES (4, 2, 10, '2023-06-05 14:26:00', 8000);
 
